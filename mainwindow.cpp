289e1315ebfa870ce5637a3922b3d3fa5ebe487f
HamidShayan1986 <hshayan1986@gmail.com>
2024-05-15 20:03:16 +0330

    First time commit to staging

---
+++ mainwindow.cpp
@@ -0,0 +1,19 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+
+//     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
+
+    // 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
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
